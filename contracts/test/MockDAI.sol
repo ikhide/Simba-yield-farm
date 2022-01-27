c8a8cfb336c3a8ca936b33dad7a77107b47e08aa
ikhide <ikhide.atakpu@gmail.com>
2022-01-27 03:41:21 +0100

    test fau staking

--- contracts/test/MockDAI.sol
+++ contracts/test/MockDAI.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockDAI is ERC20{
-    constructor() public ERC20("MockDAI","DAI"){}
+    constructor() public ERC20("MockDAI","DAI"){
+          _mint(msg.sender, 1000000000000000000000);
+    }
 }
